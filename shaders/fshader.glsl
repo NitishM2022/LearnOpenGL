00f4cd713ad765d8284e8fa5663404d53b1fe723
nitishmalluru <nitishethan@gmail.com>
2023-12-06 04:36:33 -0600

    Created succesful glowing triangle

--- shaders/fshader.glsl
+++ shaders/fshader.glsl
@@ -6,17 +6,7 @@ uniform float timeColor; // we set this variable in the OpenGL code.
 
 void main()
 {
-    const float eps = 0.00001;
-    if (abs(ourColor.r - 1.0) < eps) {
-        // If the red channel is 1.0, modify the color based on time
-        FragColor = vec4(timeColor, 0.0, 0.0, 1.0);
-    } else if (abs(ourColor.r - 1.0) < eps) {
-        // If the green channel is 1.0, modify the color differently (you can adjust this logic)
-        FragColor = vec4(0.0, timeColor, 0.0, 1.0);
-    } else if (abs(ourColor.r - 1.0) < eps) {
-        // If the blue channel is 1.0, modify the color differently (you can adjust this logic)
-        FragColor = vec4(0.0, 0.0, timeColor, 1.0);
-    } else {
-        FragColor = vec4(ourColor.r, ourColor.g, ourColor.b, 1.0);
-    }
+    // float timeColor = (sin(timeValue) / 2.0f) + 0.5f;
+    FragColor = vec4(ourColor.r*timeColor, ourColor.g*timeColor, ourColor.b*timeColor, 1.0f);      
+    //FragColor = vec4(ourColor, 1.0f)
 }
