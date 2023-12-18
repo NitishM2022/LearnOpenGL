a805be013528406dbdf7ab5ed6b17af7989bd3f5
nitishmalluru <nitishethan@gmail.com>
2023-12-18 15:05:46 -0600

    moved all intro files into specific folder

---
+++ introduction/shaders/fshader.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+#define PI 3.1415926535897932384626433832795
+out vec4 FragColor;
+
+//in vec3 ourColor;
+in vec2 TexCoord;
+uniform float timeValue; // we set this variable in the OpenGL code.
+//uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    // float timeColor = (sin(timeValue) / 2.0f) + 0.5f;
+    // float timeColor1 = (sin(timeValue /*+ 2 * PI / 3*/) / 2.0f) + 0.5f;
+    // float timeColor2 = (sin(timeValue /*+ 4 * PI / 3*/) / 2.0f) + 0.5f;
+    //FragColor = vec4(ourColor.r*timeColor, ourColor.g*timeColor1, ourColor.b*timeColor2, 1.0f);      
+    //FragColor = vec4(ourColor, 1.0f)
+    //FragColor = (sin(timeValue) / 2.0f) * texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+    //texture 1 and 2 are texture units which correpsond to textures 0 to 16;
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
---
+++ introduction/shaders/vshader.glsl
@@ -0,0 +1,20 @@
+//vertex shader is a vector of 4 length x,y,z and w
+#version 330 core
+layout (location = 0) in vec3 aPos;
+// layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec2 aTexCoord;
+
+//out vec3 ourColor;//send ourColor to next shader in pipeline
+out vec2 TexCoord;
+
+uniform mat4 model; //global model transformation
+uniform mat4 view; //camera
+uniform mat4 projection; //perspective
+
+void main()
+{
+   // note that we read the multiplication from right to left
+   gl_Position = projection * view * model * vec4(aPos, 1.0);
+   //ourColor = aColor;
+   TexCoord = aTexCoord;
+}
